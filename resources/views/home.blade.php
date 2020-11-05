8d21626019c2de0365f1b29b857d353237fc1b28
Ritchy FED <fed.ritchy@yahoo.fr>
2020-11-05 21:05:20 +0100

    test2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,9 +13,40 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
+                    You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
+ <div class='container-fluid  mld-body'>
+    <h1 class='mld-titre'> yoyoyoyoyo </h1>
+    <div class='container'>
+        <div class='row mld-div-card'>
+
+
+        @foreach ( $tables)
+
+{{-- regler soucis foreach + faire un input dans formulaires, avec @route. --}}
+
+    <div class='col-sm-5 mld-pos-card' >
+                <form >
+                 <div class='mld-card'>
+                    <div class='gneu'></div>
+
+                    <h3>{{ $table->title }}</h3>
+                    <br><a type='submit' class='mld-card-a' name='$table' value='Voir' href=''>Voir</a><br><div class='gneu'></div>
+                    <input type='hidden' value=".$table['id']." name='id'>
+
+
+                </div>
+                </form>
+            </div>
+            @endforeach
+  </div>
+    </div>
+</div>
+
+
 @endsection
