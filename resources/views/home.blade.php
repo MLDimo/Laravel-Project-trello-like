aa905d663063f7bee2c506890b76aca1596ef25c
Martin LAROCHE <martin04.laroche@gmail.com>
2020-11-05 20:56:53 +0100

    correction bug function modif profil start foreach warnig -> bug on table.blade with th routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
 
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-            </div>
         </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        @foreach ($collection as $item)
+        <div class="col-3-sm">
+
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
