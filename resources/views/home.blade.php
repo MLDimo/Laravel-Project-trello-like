367180c30eebcebd52712957cdf5c3fecc77cbdb
LeXz <lexzkid@gmail.com>
2020-11-09 15:39:34 +0100

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container-fluid lex-content">
 <div class="container-fluid">
     <div class="row">
         <div class="col">
@@ -8,7 +9,7 @@
                     @csrf
                     <label for="title"><h4> Créer un nouveau Tableau :</h4></label>
                     <input id="title" name="title" type="text" placeholder="Titre">
-                    <label for="description"> description </label>
+                    <label for="description"><h4> Description :</h4></label>
                     <input type="text" name="description" id="description">
                     <input type="submit" value="+">
                 </form>
@@ -21,36 +22,67 @@
                 <div class="card-body">
                 <h4 class="card-title"> {{ $item->title }} </h4>
                 <h6 class="card-subtitle mb-2"> {{$item->description}} </h6>
-<<<<<<< HEAD
-      <a href="#" class="card-link">Modifier</a>
-      <a href="#" class="card-link">Supprimer</a>
-=======
 
 <form method="post">
-      <a href="@route('rename.table')" class="card-link">Modifier</a>
-      <a href="@route('del.table')" class="card-link">Supprimer</a>
-      <a href="@route('table',[$item->id])" class="card-link">Voir</a>
+      <a href="@route('rename.table')" class="card-link1">Modifier</a>
+      <a href="@route('del.table')" class="card-link2">Supprimer</a>
+      <a href="@route('table',[$item->id])" class="card-link3">Voir</a>
 </form>
 
->>>>>>> 79e5863127eb79f56dfb7609f3f5c959f93ae858
             </div>
             </div>
         @endforeach
     </div>
 </div>
 </div>
-
+</div>
 @endsection
 <style>
 .card {
-    margin-left: 95px;
+    margin-left: 88px;
     margin-bottom: 50px;
 }
-    .card {
-        margin-left: 95px;
-        margin-bottom: 50px;
-    }
-
+.card-link1 {
+    color: rgb(43, 86, 226);
+    padding-left: 10px;
+    font-size: 20px;
+}
+.card-link2 {
+    color: crimson;
+    padding-left: 10px;
+    font-size: 20px;
+}
+.card-link3 {
+    color: green;
+    padding-left: 10px;
+    font-size: 20px;
+}
+.card-title {
+    color: rgb(0, 0, 0);
+    background-position: center;
+    font-size: 30px;
+    padding-bottom: 25px;
+    text-align: center;
+}
+.card-subtitle {
+    padding-bottom:30px;
+    font-size: 18px;
+    font-style: italic;
+    text-align: center;
+}
+.container-fluid {
+    text-align: center;
+}
+h4 {
+    color: rgb(0, 0, 0);
+}
+.py-4 {
+    background-color: rgb(168, 167, 110);
+}
+.col {
+    margin-top: 30px;
+    margin-bottom: 30px;
+}
 </style>
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('Genial', 'Genial') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,10 +21,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('Accueil', 'Accueil') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -79,5 +79,10 @@
             @yield('content')
         </main>
     </div>
+    <style>
+        body {
+        background-color: rgb(168, 167, 110);
+        }
+    </style>
 </body>
 </html>
